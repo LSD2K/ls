4d87943766ab85a3c29b208a36a3a0a42f005b96
LSD2K <breiin00@gmail.com>
2023-11-12 22:56:53 +0300

    new

--- st.go
+++ st.go
@@ -1,7 +1,10 @@
-package main
+/*package main
 
 import "fmt"
 
 func main() {
 	fmt.Println("Hello Go!")
+	fmt.Println("My name is nikita!")
+	fmt.Println("and you?")
 }
+*/
---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hello string = "fuck you bitch"
+	var a, b, c int
+	a = 18
+	b = 28
+	c = 1 + 4
+	fmt.Println(a, b, c, hello)
+}
