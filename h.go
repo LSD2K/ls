9531ab848271b0159b26d03148314c5484895865
LSD2K <breiin00@gmail.com>
2023-11-13 13:08:17 +0300

    new

---
+++ h.go
@@ -0,0 +1,40 @@
+package main
+
+func main() {
+
+	var a int = 5
+	switch a {
+	case 6:
+		println("eto 6")
+	case 7:
+		println("eto 7")
+	case
+		
+	}
+
+	/*var a, b, c int = 5, 6, 11
+
+	if a > b || a+b == c {
+		fmt.Println("a bolshe b\n")
+	} else if a < b {
+
+		fmt.Println("a ravno b\n")
+	} else {
+		fmt.Println("a menshe b\n")
+	}
+
+	var num [5]int64 = [5]int64{}
+
+	fmt.Println(num[4])
+
+	var a, b, c int = 5, 6, 7 & 2
+	var (
+		name string = "nikita\n"
+		age  int    = 22
+	)
+	happy := c > b
+	const (
+		pi float64 = 3.1415
+	)
+	fmt.Println(name, age, "\n", happy, "\n", pi, "\n", a, "\n", b, "\n", c, "\n", a*b*c)*/
+}
